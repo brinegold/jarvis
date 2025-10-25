c4ed943c48d747f03f0b3949df4b7db178cd8729
areebasiddiqi <hamidsiddiqi888@gmail.com>
2025-10-25 09:50:13 +0500

    updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -495,7 +495,7 @@ export default function DashboardPage() {
                 <Youtube className="h-4 w-4 sm:h-5 sm:w-5" />
               </a>
               <a 
-                href="mailto:jarvissupport@atomicmail.io" 
+                href="https://t.me/+vIW_s8xt3IdmNjg0" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="p-1.5 sm:p-2 text-white hover:bg-blue-500/20 hover:text-blue-400 rounded-full transition-all duration-300"
